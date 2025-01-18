1a666c14f5f24f5b29a604eb533aea1cecc448ac
Noah Allen <noahallen@Noahs-MacBook-Pro.local>
2025-01-18 13:14:12 -0500

    menu set up

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using Product;
 
-class Program
+
+namespace Reductio
 {
-  public static void Main()
+  
+public class Program
+{
+  
+  List<Products> products = new List<Products>
   {
 
+  }; 
+
+  public static void Main()
+  {
+    int userChoice;
+
+    do
+    {
+    //Main Menu
+    Console.WriteLine("Welcome To Reductio & Absurdum!");
+    Console.WriteLine("=============Main Menu=============");
+    Console.WriteLine(" 1. View All Products \n 2. Add a New Product \n 3. Update a Product \n 4. Remove a Product \n 5. Quit");
+    Console.WriteLine("===================================");
+
+    userChoice = int.Parse(Console.ReadLine());
+
+    
+    //CLOSE Main Menu
+
+    } while (userChoice != 5);
+    
   }//close Main
 
 } //close class
+
+}// close namespace
