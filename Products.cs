5e173f50b58f9a954e83e4cf6dc29fd2741da31e
Noah Allen <noahallen@Noahs-MacBook-Pro.local>
2025-01-18 14:25:24 -0500

    menu is set up with options

--- Products.cs
+++ Products.cs
@@ -2,5 +2,17 @@ namespace Reductio;
 
 public class Products
 {
+  public string Name { get; set; }
+  public decimal Price { get; set; }
+  public bool IsAvailable { get; set; }
+  public int ProductId { get; set; }
+  public ProductType ProductType { get; set; }
 
+
+}
+
+public class ProductType
+{
+  public string Name { get; set;}
+  public int Id { get; set;}
 }
