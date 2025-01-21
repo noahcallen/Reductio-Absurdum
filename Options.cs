b014b67ff35230cbdf0bd0e8125f4df4adcff593
Noah Allen <noahallen@Noahs-MacBook-Pro.local>
2025-01-21 16:44:19 -0500

    case 1 finished (display all products)

--- Options.cs
+++ Options.cs
@@ -1,30 +1,40 @@
+
+using Reductio;
+using MainConsole;
+using System;
+
 namespace Options;
 
 public class MenuFunction
 {
   public int userChoice;
 
-  public static void choice1()
+  public static void choice1(List<Products> products)
   {
-    Console.WriteLine("1");
+    Console.WriteLine("All Products: ");
+    foreach (var product in products)
+    {
+      Console.WriteLine($"- {product.Name}");
+    }//close foreach
+    Console.WriteLine("-----------------------------------");
   }//close choice1
 
-  public static void choice2()
+  public static void choice2(List<Products> products)
   {
     Console.WriteLine("2");
   }//close choice2
 
-  public static void choice3()
+  public static void choice3(List<Products> products)
   {
     Console.WriteLine("3");
   }//close choice3
 
-  public static void choice4()
+  public static void choice4(List<Products> products)
   {
     Console.WriteLine("4");
   }//close choice4
 
-  public static void choice5()
+  public static void choice5(List<Products> products)
   {
     Console.WriteLine("5");
   }//close choice5
